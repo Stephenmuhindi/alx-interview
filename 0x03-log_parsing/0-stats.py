9578d0309349a21a1a686131bf92e75cdf0f96ea
stephenmuhindi <bteneson42@gmail.com>
2024-05-16 19:35:06 +0300

    al

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,62 +1,39 @@
 #!/usr/bin/python3
-"""
-Simulates random web server access logs for 10,000 requests.
-
-This script generates and prints simulated web server access log entries
-in a common format. It includes the following details for each request:
-
-- IP address (randomized)
-- Timestamp (current date and time)
-- HTTP method (GET)
-- Requested resource path (/projects/260)
-- HTTP version (HTTP/1.1)
-- Status code (randomly chosen from common codes)
-- Content size (randomized)
-
-The script iterates 10,000 times, generating a new log entry with random
-values for each iteration. It uses sleep statements to introduce random
-delays between simulated requests.
-"""
-import random
-import sys
-from time import sleep
-import datetime
 
 
-def main():
-    """
-    The main function for generating simulated web server access logs.
-
-    This function iterates 10,000 times, generating and printing a single
-    simulated access log entry in each iteration.
-    """
-
-    for _ in range(10000):
-        # Simulate random delay between requests
-        sleep(random.random())
-
-        # Generate random IP address components
-        ip1 = random.randint(1, 255)
-        ip2 = random.randint(1, 255)
-        ip3 = random.randint(1, 255)
-        ip4 = random.randint(1, 255)
-
-        # Get current date and time
-        timestamp = datetime.datetime.now()
-
-        # Choose a random HTTP status code
-        status_code = random.choice([200, 301, 400, 401, 403, 404, 405, 500])
-
-        # Generate random content size
-        content_size = random.randint(1, 1024)
-
-        # Format and print the log entry
-        log_entry = "{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}".format(
-            ip1, ip2, ip3, ip4, timestamp, status_code, content_size
-        )
-        print(log_entry, flush=True)
+def parseLogs():
+    stdin = __import__('sys').stdin
+    lineNumber = 0
+    fileSize = 0
+    statusCodes = {}
+    codes = ('200', '301', '400', '401', '403', '404', '405', '500')
+    try:
+        for line in stdin:
+            lineNumber += 1
+            line = line.split()
+            try:
+                fileSize += int(line[-1])
+                if line[-2] in codes:
+                    try:
+                        statusCodes[line[-2]] += 1
+                    except KeyError:
+                        statusCodes[line[-2]] = 1
+            except (IndexError, ValueError):
+                pass
+            if lineNumber == 10:
+                report(fileSize, statusCodes)
+                lineNumber = 0
+        report(fileSize, statusCodes)
+    except KeyboardInterrupt as e:
+        report(fileSize, statusCodes)
+        raise
 
 
-if __name__ == "__main__":
-    main()
+def report(fileSize, statusCodes):
+    print("File size: {}".format(fileSize))
+    for key, value in sorted(statusCodes.items()):
+        print("{}: {}".format(key, value))
 
+
+if __name__ == '__main__':
+    parseLogs()
