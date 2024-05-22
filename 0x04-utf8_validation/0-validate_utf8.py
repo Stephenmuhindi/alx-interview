360efd76d53eaa44c1ceeb823ecd000a7709ad19
Stephenmuhindi <bteneson42@gmail.com>
2024-05-22 16:01:16 -0700

    com

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """
-utf8-valid
+check for utf8
 """
 
 
 def validUTF8(data):
-    """utf-8 validation check"""
-    expected_continuations = 0
-    for byte in data:
-        if expected_continuations == 0:
-            if (byte >> 5) == 0b110:
-                expected_continuations = 1
-            elif (byte >> 4) == 0b1110:
-                expected_continuations = 2
-            elif (byte >> 3) == 0b11110:
-                expected_continuations = 3
-            elif (byte >> 7):
+    """
+    return true or false
+    """
+    byte_count = 0
+
+    for i in data:
+        if byte_count == 0:
+            if i >> 5 == 0b110 or i >> 5 == 0b1110:
+                byte_count = 1
+            elif i >> 4 == 0b1110:
+                byte_count = 2
+            elif i >> 3 == 0b11110:
+                byte_count = 3
+            elif i >> 7 == 0b1:
                 return False
         else:
-            if (byte >> 6) != 0b10:
+            if i >> 6 != 0b10:
                 return False
-            expected_continuations -= 1
-    return expected_continuations == 0
+            byte_count -= 1
+    return byte_count == 0
