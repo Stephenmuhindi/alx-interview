769280a217217db1805ab73cb7b6e96522e71be8
Stephenmuhindi <bteneson42@gmail.com>
2024-06-26 04:54:58 -0700

    done

---
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""
+module documentation
+"""
+
+bound_4 = set()
+bound_3 = set()
+bound_2 = set()
+bound_1 = set()
+
+
+def boundary(grid, i, j):
+    """
+    This function checks the surrounding cells
+    of a given cell (i, j) in the grid.
+    It counts the number of neighboring cells that are water (0).
+    Based on the count (boundaries).
+    """
+    boundaries = 0
+    try:
+        if i == 0:
+            boundaries += 1
+        elif grid[i-1][j] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+    try:
+        if grid[i+1][j] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+    try:
+        if grid[i][j+1] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+    try:
+        if j == 0:
+            boundaries += 1
+        elif grid[i][j-1] == 0:
+            boundaries += 1
+    except:
+        boundaries += 1
+
+    if boundaries == 1:
+        bound_1.add((i, j))
+    elif boundaries == 2:
+        bound_2.add((i, j))
+    elif boundaries == 3:
+        bound_3.add((i, j))
+    elif boundaries == 4:
+        bound_4.add((i, j))
+
+
+def island_perimeter(grid):
+    """
+    It iterates through the grid 
+    calls boundary for each land cell (1).
+    If a cell with 4 exposed boundaries (bound_4) is found,
+    it immediately returns 4 (entirely surrounded cell has a perimeter of 4).
+    """
+    if grid == []:
+        return 0
+    l = len(grid)
+    w = len(grid[0])
+    for i in range(l):
+        for j in range(w):
+            if grid[i][j] == 1:
+                boundary(grid, i, j)
+                if len(bound_4) != 0:
+                    return 4
+    perimeter = (len(bound_3) * 3) + (len(bound_2) * 2) + (len(bound_1))
+    return perimeter
