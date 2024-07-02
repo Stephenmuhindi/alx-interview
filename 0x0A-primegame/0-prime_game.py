2dadb15efed84da34bb923cee277b3173bd753ec
Stephenmuhindi <bteneson42@gmail.com>
2024-07-02 04:24:49 -0700

    task12

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,38 +1,42 @@
 #!/usr/bin/python3
-"""
-module description
-"""
-
-
-def primes(n):
-    """
-    return prime numeral from 1 to the num
-    """
-    prime = []
-    sieve = [True] * (n + 1)
-    for p in range(2, n + 1):
-        if (sieve[p]):
-            prime.append(p)
-            for i in range(p, n + 1, p):
-                sieve[i] = False
-    return prime
+""" module documentation"""
 
 
 def isWinner(x, nums):
     """
-    who is the winner of the game?
+    lets see who wins
     """
-    if x is None or nums is None or x == 0 or nums == []:
+    if x <= 0 or nums is None:
         return None
-    Maria = Ben = 0
-    for i in range(x):
-        prime = primes(nums[i])
-        if len(prime) % 2 == 0:
-            Ben += 1
+    if x != len(nums):
+        return None
+
+    ben = 0
+    maria = 0
+
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for i in range(2, len(a)):
+        rm_multiples(a, i)
+
+    for i in nums:
+        if sum(a[0:i + 1]) % 2 == 0:
+            ben += 1
         else:
-            Maria += 1
-    if Maria > Ben:
-        return 'Maria'
-    elif Ben > Maria:
-        return 'Ben'
+            maria += 1
+    if ben > maria:
+        return "Ben"
+    if maria > ben:
+        return "Maria"
     return None
+
+
+def rm_multiples(ls, x):
+    """
+    delete siame value
+    """
+    for i in range(2, len(ls)):
+        try:
+            ls[i * x] = 0
+        except (ValueError, IndexError):
+            break
